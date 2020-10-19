a2095862d9d236c53decacbd689df8b7a26e9f83
Eduardo Bernardino <eduardomoura.moura@gmail.com>
2020-10-19 17:09:48 -0300

    API Integration

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 function Home() {
+  const [panettones, setPanettones] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const datas = await api.get('/panettones');
+      setPanettones(datas.data);
+    }
+
+    getData();
+  }, []);
   return (
-    <h1>Home</h1>
+    <div>{panettones.map(p => (
+      <div key={p.id}>
+        <p>{p.id}</p>
+        <p>{p.name}</p>
+      </div>
+
+    ))
+    }</div>
   );
 }
 
